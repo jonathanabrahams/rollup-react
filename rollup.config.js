f2386b799e2308a4cef40c42347754024146dc74
Jonathan Abrahams <jonathanabrahams@gmail.com>
2018-09-30 00:15:48 +0200

    Add redux

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,15 @@ import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'build/index.js',
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'redux'],
   output: {
     file: 'public/build/bundle.js',
     format: 'iife',
     name: 'App',
     globals: {
       'react': 'React',
-      'react-dom': 'ReactDOM'
+      'react-dom': 'ReactDOM',
+      'redux': 'Redux'
     }
   },
   plugins: [
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
 import * as React from "react"
 import * as ReactDOM from 'react-dom';
+import * as Redux from 'redux';
 import './index.css';
+
+const rootReducer = (state = {
+    counter:0
+}, action: Redux.Action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+/* eslint-disable no-underscore-dangle */
+const store = Redux.createStore(
+    rootReducer,
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 export const MyFunction = () => <div>Hello World!12</div>
 
